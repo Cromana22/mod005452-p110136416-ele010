9f18d2e8b89e48d9a83a0c0b4dd21a7ff2eeb5c3
P110136416 <p110136416@student.peterborough.ac.uk>
2023-02-06 15:32:44 +0000

    working on dropdown menu

--- Flexitodo/app/src/main/java/com/example/flexitodo/MainActivity.kt
+++ Flexitodo/app/src/main/java/com/example/flexitodo/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.flexitodo
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.example.flexitodo.ui.theme.FlexitodoTheme
 import com.example.flexitodo.*
@@ -37,23 +43,53 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun Layout() {
         Scaffold(
+            topBar = { TopAppBarSample() },
             content = {
-                Column {
-                    Row {
-                        Greeting("Main Page")
-                    }
-                    Row {
-                        NavButton(page = Settings::class.java, text = "Settings")
-                        NavButton(page = AddTodo::class.java, text = "Add New")
-                    }
+                Row {
+                    NavButton(page = Settings::class.java, text = "Settings")
+                    NavButton(page = AddTodo::class.java, text = "Add New")
                 }
             }
         )
     }
 
     @Composable
-    fun Greeting(name: String) {
-        Text(text = "Hello $name!")
+    fun TopAppBarSample() {
+        TopAppBar(
+            title = {
+                val expanded = remember{ mutableStateOf(false) }
+
+                DropdownMenu(
+                    expanded = expanded.value,
+                    onDismissRequest = { expanded.value = false }
+                ) {
+                    DropdownMenuItem(
+                        text = { Text("Todo List 1") },
+                        onClick = { /*TODO*/ }
+                    )
+                    DropdownMenuItem(
+                        text = { Text("Todo List 2") },
+                        onClick = { /*TODO*/ }
+                    )
+                }
+            },
+            /*navigationIcon = {
+                IconButton(onClick = {/* Do Something*/ }) {
+                    Icon(Icons.Filled.ArrowBack, null)
+                }
+            }, */
+            actions = {
+                IconButton(onClick = {/* Do Something*/ }) {
+                    Icon(Icons.Filled.Notifications, null)
+                }
+                IconButton(onClick = {/* Do Something*/ }) {
+                    Icon(Icons.Filled.Settings, null)
+                }
+                IconButton(onClick = {/* Do Something*/ }) {
+                    Icon(Icons.Filled.Add, null)
+                }
+            }
+        )
     }
 
     @Composable
